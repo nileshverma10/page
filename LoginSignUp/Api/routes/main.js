c2898ae456fb06018f0c71be8609b1902be08352
nileshverma10 <100075212+nileshverma10@users.noreply.github.com>
2023-03-11 19:28:13 +0530

    correction

--- LoginSignUp/Api/routes/main.js
+++ LoginSignUp/Api/routes/main.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const data = require("../controller/main");
+const express = require('express')
+const router = express.Router()
+const data = require('../controller/main')
 
-router.post("/register", data.register);
-router.get("/getData", data.getData);
-router.delete("/deleteData/:id", data.deleteData);
-router.patch("/patchData/:id", data.patchData);
-router.post("/login", data.login);
-router.post("/addProduct", data.addProduct);
-router.get("/getProduct", data.getProduct);
-router.delete("/deleteList/:id", data.deleteList);
-router.get("/getProductDetail/:id", data.getProductDetail);
-// router.put('/updateProduct/:id',data.updateProductDetail)
-router.put("/updateProduct/:id", data.updateProduct);
+router.post('/register', data.register)
+router.get('/getData', data.getData)
+router.delete('/deleteData/:id', data.deleteData)
+router.patch('/patchData/:id', data.patchData)
+router.post('/login', data.login)
+router.post('/addProduct', data.addProduct)
+router.get('/getProduct', data.getProduct)
+router.delete('/deleteList/:id', data.deleteList)
+router.get('/getProductDetail/:id', data.getProductDetail)
+router.put('/updateProduct/:id', data.updateProduct)
 
-module.exports = router;
+module.exports = router
